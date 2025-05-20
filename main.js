358f42d1c589dac033f4013001855f131057505e
taylanhutt <160601352+taylanhutt@users.noreply.github.com>
2025-05-20 09:48:07 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -13,6 +13,24 @@ botoes [i].onclick = function() {
 }
 }
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date(2025-05-05)
+const tempoObjetivo1 = new Date("2025-07-05T00:00:00");
+const tempoObjetivo2 = new Date("2025-10-05T00:00:00");
+const tempoObjetivo3 = new Date("2025-09-05T00:00:00");
+const tempoObjetivo4 = new Date("2026-01-05T00:00:00");
 
-contadores[0].textContent = tempoObjetivo1;
+contadores[0].textContent = calculaTempo(tempoObjetivo1)
+
+function calculaTempo(tempoObjetivo){ 
+    let tempoAtual = new Date();
+    let tempofinal = tempoObjetivo1 - tempoAtual;
+    let segundos = Math.floor(tempofinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(minutos/60);
+    let dias = Math.floor(horas/24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+}
+ 
