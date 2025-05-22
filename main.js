ccb493fd5ed25427742db940859f45b2a91fdd61
taylanhutt <160601352+taylanhutt@users.noreply.github.com>
2025-05-22 11:48:54 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -13,14 +13,15 @@ botoes [i].onclick = function() {
 }
 }
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2025-07-05T00:00:00");
+const tempoObjetivo1 = new Date("2020-07-05T00:00:00");
 const tempoObjetivo2 = new Date("2025-10-05T00:00:00");
 const tempoObjetivo3 = new Date("2025-09-05T00:00:00");
 const tempoObjetivo4 = new Date("2026-01-05T00:00:00");
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1)
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
-function calculaTempo(tempoObjetivo){ 
+
+function calculaTempo(tempoObjetivo) { 
     let tempoAtual = new Date();
     let tempofinal = tempoObjetivo1 - tempoAtual;
     let segundos = Math.floor(tempofinal/1000);
@@ -31,6 +32,23 @@ function calculaTempo(tempoObjetivo){
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
-    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+    if (tempoFinal > 0){
+        
+        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+    } else {
+        return "Prazo finalizado";
+    }
+    }
+
+function atualizaCronometro(){ 
+    for (let i=0; i<contadores.length;i++){
+          contadores[i].textContent = calculaTempo(tempos[i]);
+    }
+    }
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro,1000);
 }
+
+comecaCronometro();
  
